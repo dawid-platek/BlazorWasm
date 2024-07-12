ccd20f0c7c24dfc84165ff06e00ada9f07dfd72a
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-12 23:05:33 +0200

    Module 2 lesson 6 - separation of responsibilities

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace BlazorWasm.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private bool _isActive = false;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        private void ToogleActive()
+        {
+            _isActive = !_isActive;
+        }
+    }
+}
