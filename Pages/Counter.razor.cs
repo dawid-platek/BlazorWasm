37ec95afb9d3516303d436292f412a3090116926
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-07-13 21:11:05 +0200

    Module 2 lesson 9 - routing, navigation manager

--- Pages/Counter.razor.cs
+++ Pages/Counter.razor.cs
@@ -1,11 +1,25 @@
 ﻿
 
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorWasm.Pages
 {
     public partial class Counter
     {
         private int currentCount = 0;
         private bool _isActive = false;
+
+        [Parameter]
+        public int Header { get; set; }
+
+        [Parameter]
+        public bool? IsDeleted { get; set; }
+
+        [SupplyParameterFromQuery(Name = "Header2")]
+        public string Header2 { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
 
         private void IncrementCount()
         {
@@ -15,6 +29,11 @@ namespace BlazorWasm.Pages
         private void ToogleActive()
         {
             _isActive = !_isActive;
+        }
+
+        private void GoToIndex()
+        {
+            NavigationManager.NavigateTo("/counter-par/99/true");
         }
     }
 }
