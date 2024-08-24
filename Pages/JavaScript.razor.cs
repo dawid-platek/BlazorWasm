27d9812c5e2b559f0a03a7891502ba4df678f459
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-08-24 23:34:42 +0200

    Module 3 lesson 6 - using JavaScript in Blazor

---
+++ Pages/JavaScript.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorWasm.Pages
+{
+
+    public partial class JavaScript
+    {
+        private bool _dialogResult = false;
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        private async Task DisplayAlert()
+        {
+            await JSRuntime.InvokeVoidAsync("alert", "Przykładowa wiadomość");
+        }
+
+        private async Task DisplayConfirmDialog()
+        {
+            _dialogResult = await JSRuntime.InvokeAsync<bool>("confirm", "Czy na pewno chcesz usunąć rekord?");
+        }
+
+        private async Task ShowResultJs()
+        {
+            await JSRuntime.InvokeVoidAsync("addNumberJS", 1, 2);
+        }
+    }
+}
